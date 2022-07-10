9507a8e805b02982e89f10ea1bf4a69fb9c43c50
SamanthaRiver <106163754+SamanthaRiver@users.noreply.github.com>
2022-07-10 00:08:59 -0400

    Add files via upload

---
+++ PAssign2/cmake-build-debug/CMakeFiles/PAssign2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAssign2.dir/main.c.o"
+  "PAssign2"
+  "PAssign2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PAssign2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
